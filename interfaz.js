1a37ed42df0218ab9f19c52bd321cfb1aa2178e5
Federico Aldrighetti <56283793+faldrighetti@users.noreply.github.com>
2022-07-04 20:45:32 -0300

    Sube archivos del programa
    Falta corregir bugs y agregar funcionalidad de texto al costado con bootstrap

---
+++ interfaz.js
@@ -0,0 +1,105 @@
+const $simulateButton = document.querySelector('#simulate-button')
+const $deleteButton = document.querySelector('#delete-button')
+
+const finalTable = document.querySelector('#final-standings')
+
+function destroyButton(){
+    $simulateButton.className = 'hidden'
+}
+
+function createDeleteButton(){
+    $deleteButton.className = ''
+}
+
+$deleteButton.onclick = function(){
+    clearPrevious()
+    
+    $simulateButton.className = ''
+    $deleteButton.className = 'hidden'
+}
+
+$simulateButton.onclick = function(){
+
+    const textTeams = document.querySelectorAll('.teams')
+    if (textTeams.length){
+        clearPrevious()
+    }
+
+    simulateLeague()
+    placeTeams()
+    destroyButton()
+    createDeleteButton()
+}
+
+function clearPrevious(){
+    const textTeams = document.querySelectorAll('section')
+    const hrLines = document.querySelectorAll('hr')
+
+    for (let i = 0; i < textTeams.length; i++){
+        textTeams[i].remove()
+    }
+
+    for (let i = 0; i < hrLines.length; i++){
+        hrLines[i].remove()
+    }
+}
+
+function placeTeams(){
+
+    let finalStandings = simulateLeague()
+
+    for (i = 0; i < finalStandings.length; i++){
+        const newDiv = document.createElement('div')
+        const nextTeam = document.createElement('section')
+        nextTeam.textContent = Number(i + 1) + '. ' + finalStandings[i][0] + ' - ' + finalStandings[i][1] + ' points';
+
+        if(i === 0){
+            nextTeam.id = 'winner'
+        }
+        else if(i === 4){
+            let spaceSection = document.createElement('p')
+            let championsLine = document.createElement('hr')
+            championsLine.className = 'championsLine'
+            newDiv.appendChild(championsLine)
+            newDiv.appendChild(spaceSection)
+        }
+        else if(i === 6){
+            let europaLine = document.createElement('hr')
+            europaLine.className = 'europaLine'
+            newDiv.appendChild(europaLine)
+        }
+        else if(i === 7){
+            let conferenceLine = document.createElement('hr')
+            conferenceLine.className = 'conferenceLine'
+            newDiv.appendChild(conferenceLine)
+        }
+        else if (i === 17){
+            let relegation = document.createElement('hr')
+            relegation.className = 'relegation'
+            newDiv.appendChild(relegation)
+        }
+
+        finalTable.appendChild(newDiv)
+        newDiv.appendChild(nextTeam)
+    }
+}
+
+/*
+Restantes:
+-XXXXOrdenar clubes por puntos de mayor a menor
+-XXXXCrear un div para pasar resultados a texto en la página
+-Ver qué pasa si hay igualdad en puntos (lo definiría al azar)
+-XXXXHacer algo para limpiar, que NO sea recargar la página. No que borre y vuelva a simular de una, sino que vuelva a cero: Borrar el div
+-Hacer texto para campeón, copas europeas y descenso. Todo a la derecha, con bootstrap. Ejemplo:
+
+
+1-Liverpool, 100                    Winner: Liverpool
+2-Manchester City, 95               UEFA Champions League: Liverpool, Manchester City, Chelsea
+3-Chelsea, 81                       and Arsenal
+4-Arsenal, 75                       UEFA Europa League: Manchester United and Tottenham
+5-Manchester United, 73             UEFA Conference League: Leicester City
+6-Tottenham, 71                     Relegation: Burnley, Watford and Norwich City
+7-Leicester City, 67
+...
+20-Norwich City, 31
+*/
---
+++ simulador-liga.js
@@ -0,0 +1,179 @@
+const teams = ['Chelsea', 'Liverpool', 'Manchester City','Arsenal', 'Manchester United', 'Tottenham Hotspur', 'Leicester City', 
+'West Ham','Everton', 'Aston Villa', 'Newcastle United', 'Southampton', 'Brighton', 'Crystal Palace', 'Wolverhampton','Leeds United',
+'Brentford', 'Norwich City', 'Watford', 'Burnley']
+
+const powerhouse = ['Chelsea', 'Liverpool', 'Manchester City']
+const challenger = ['Arsenal', 'Manchester United', 'Tottenham Hotspur', 'Leicester City']
+const midtable = ['Aston Villa', 'Brighton', 'Crystal Palace', 'Everton', 'Newcastle United', 'Southampton','West Ham', "Wolverhampton"]
+const survival = [ 'Brentford', 'Burnley', 'Leeds United', 'Norwich City', 'Watford']
+
+const matches = 38
+const possibilities = 100
+
+const powerhouseWin = 70
+const powerhouseDraw = 85
+const challengerWin = 50
+const challengerDraw = 75
+const midtableWin = 34
+const midtableDraw = 67
+const survivalWin = 25
+const survivalDraw = 65
+
+function throwDice(){
+    return Math.floor(Math.random() * possibilities) + 1; // dado 1-100
+}
+
+function simulatePowerhouse(){
+    
+    let wins = 0
+    let draws = 0
+    let losses = 0
+    let points = 0
+
+        for (let i = 0; i < matches; i++){
+            let result = throwDice()
+            if (result <= powerhouseWin){
+                wins++;
+                points +=3;
+            }
+            else if(result > powerhouseWin && result <= powerhouseDraw){
+                draws++;
+                points++;
+            }
+            else{
+                losses++;
+            }
+        }
+    return points
+}
+
+function simulateChallenger(){
+
+    let wins = 0
+    let draws = 0
+    let losses = 0
+    let points = 0
+
+    for (let i = 0; i < matches; i++){
+        let result = throwDice()
+        if (result <= challengerWin){
+            wins++;
+            points += 3;
+        }
+        else if(result > challengerWin && result <= challengerDraw){
+            draws++;
+            points++;
+        }
+        else{
+            losses++;
+        }
+    }
+    return points
+}
+
+function simulateMidtable(){
+    
+    let wins = 0
+    let draws = 0
+    let losses = 0
+    let points = 0
+
+    for (let i = 0; i < matches; i++){
+        let result = throwDice()
+        if (result <= midtableWin){
+            wins++;
+            points +=3;
+        }
+        else if(result > midtableWin && result <= midtableDraw){
+            draws++;
+            points++;
+        }
+        else{
+            losses++;
+        }
+    }
+    return points
+}
+
+function simulateSurvival(){
+    
+    let wins = 0
+    let draws = 0
+    let losses = 0
+    let points = 0
+
+    for (let i = 0; i < matches; i++){
+        let result = throwDice()
+        if (result <= survivalWin){
+            wins++;
+            points +=3;
+        }
+        else if(result > survivalWin && result <= survivalDraw){
+            draws++;
+            points++;
+        }
+        else{
+            losses++;
+        }
+    }
+    return points
+}
+
+function simulateLeague(){
+
+    let arrayPoints = []
+
+    for (let i = 0; i < teams.length; i++){
+        for (let j = 0; j < powerhouse.length; j++){
+            if (teams[i] === powerhouse[j]){
+                let amount = simulatePowerhouse()
+                console.log(teams[i] + ' - ' + amount + ' points.')
+                arrayPoints.push([teams[i], amount]);
+            }
+        }
+        for (let j = 0; j < challenger.length; j++){
+            if (teams[i] === challenger[j]){
+                let amount = simulateChallenger()
+                console.log(teams[i] + ' - ' + amount + ' points.')
+                arrayPoints.push([teams[i], amount]);
+            }
+        }
+        for (let j = 0; j < midtable.length; j++){
+            if (teams[i] === midtable[j]){
+                let amount = simulateMidtable()
+                console.log(teams[i] + ' - ' + amount + ' points.')
+                arrayPoints.push([teams[i], amount]);
+            }
+        }
+        for (let j = 0; j < survival.length; j++){
+            if (teams[i] === survival[j]){
+                let amount = simulateSurvival()
+                console.log(teams[i] + ' - ' + amount + ' points.')
+                arrayPoints.push([teams[i], amount]);
+            }
+        }
+    }
+
+    let finalStandings = arrayPoints.sort(([a, b], [c, d]) => d - b);
+    console.log(finalStandings)
+    return finalStandings;
+    
+}
+
+/*  ESTE BLOQUE DE CÓDIGO SE USA PARA ORDENAR EL ARRAY DE NÚMEROS, PERO QUIERO
+ENTENDER QUÉ HACE ANTES DE MANDARLO AL CÓDIGO
+function compareNumbers(a, b) {
+    return a - b;
+}
+
+console.log(compareNumbers(7,4)) // devuelve 3
+
+*/
+
+function untie(){
+    // En caso de desempate, poner a los equipos igualados en puntos en una lista, hacerles tirar un dado y el más alto va primero.
+    // Porque actualmente, el sistema pone directamente al primero en la lista de equipos, hay que corregir eso
+    // Posible bug: Liverpool y City igualados con 90 puntos, Watford y Norwich igualados con 40. Que no queden los 4 en la misma lista.
+    // Tendría que ser un doble for cuadrático creo, espero que no.
+    // Me conviene un diccionario
+}
